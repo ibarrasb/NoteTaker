2e2bfa27e2ee081a1f02e4553e61485f5b41eabb
Eduardo Ibarra <eddieibarra43@gmail.com>
2020-12-19 14:50:41 -0600

    html redirect working

---
+++ redirect/note.js
@@ -0,0 +1,5 @@
+
+let data = require('../db/db.json');
+var fs = require('fs');
+var path = require('path');
+
--- server.js
+++ server.js
@@ -2,15 +2,21 @@ var express = require("express");
 
 var app = express();
 
-var PORT = process.env.PORT ||3000;
+var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
+// set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use (express.static('public'));
 
-require("./redirect/note")(app);
-require("./redirect/html")(app);
+// tell the server to fetch assets from public folder
+app.use(express.static('public'));
 
+// ROUTES to point the server to
+// tells the server how to respond when users visit or request data from various URLs
+require('./redirect/note')(app);
+require('./redirect/html')(app);
+
+// start the Express server
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log('App listening on PORT: ' + PORT);
 });
